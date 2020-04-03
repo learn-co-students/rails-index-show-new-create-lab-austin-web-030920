16668973e3f3d7e08d1bf85045846b67f9a3f9c0
Tr1ckC0 <trick.co@yahoo.com>
2020-04-03 16:32:28 -0500

    lab finished

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,12 +4,21 @@ class CouponsController < ApplicationController
   end
 
   def show
+    set_coupon
   end
 
   def new
   end
 
   def create
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+    redirect_to coupon_path(@coupon)
+  end
+
+  private
+
+  def set_coupon
+    @coupon = Coupon.find(params[:id])
   end
 
 end
