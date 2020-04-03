bc5c9d6e532be10b15c35ec728bcdf58e19d9a73
Tr1ckC0 <trick.co@yahoo.com>
2020-04-03 16:19:00 -0500

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-coupons = Coupon.create([{coupon_code: }])
+10.times {Coupon.create({ coupon_code: Faker::Number.number(digits: 10), store: Faker::App.name })}
